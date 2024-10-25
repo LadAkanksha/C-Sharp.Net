9d8dfc2b4359bee275071f59b00ab6006ab770d4
LadAkanksha <akankshalad2003@gmail.com>
2024-10-25 22:26:20 +0530

    Console Program

---
+++ Console Based Program/Implicit_Conversion/Implicit_Conversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1999;
+            long l = i;
+            float f = l;
+            double d = f;
+
+            Console.WriteLine("Int Value:" + i);
+            Console.WriteLine("Long value:" + l);
+            Console.WriteLine("Float value:" + f);
+            Console.WriteLine("Double value:" + d);
+     
+            Console.ReadKey();
+        }
+    }
+}
