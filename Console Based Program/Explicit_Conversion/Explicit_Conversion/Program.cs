4dad027bb50c01dab9c5cbb4ef3a81da22e31ddf
LadAkanksha <akankshalad2003@gmail.com>
2024-10-25 22:27:25 +0530

    Console Program

---
+++ Console Based Program/Explicit_Conversion/Explicit_Conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 8526.45;
+            float f = (float)d;
+            long l = (long)f;
+            int i = (int)l;
+            Console.WriteLine("Value in Double type : " + d);
+            Console.WriteLine("Value in Float type : " + f);
+            Console.WriteLine("Value in Long type : " + l);
+            Console.WriteLine("Value in Int type : " + i);
+            Console.ReadKey();
+        }
+    }
+}
