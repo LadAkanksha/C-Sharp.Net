1a352ac99fe347368d135371b552a81e7ade1056
LadAkanksha <akankshalad2003@gmail.com>
2024-10-26 22:50:55 +0530

    Console Program

---
+++ Console Based Program/Single_Inheritance/Single_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Inheritance
+{
+    class Employee
+    {
+        public float salary = 40000;
+    }
+    class Programmer : Employee
+    {
+        public float bouns = 10000;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bouns);
+            Console.ReadKey();
+        }
+    }
+}
