b7ee0af3e9312d3d0b56342c1927ce7b3fe25395
LadAkanksha <akankshalad2003@gmail.com>
2024-10-27 21:22:01 +0530

    Console Program

---
+++ Console Based Program/PassByReference/PassByReference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByReference
+{
+    class Program
+    {
+        static void square(ref int val)
+        {
+            val = val * val;
+            Console.WriteLine("\nReference Value in Function : " + val);
+        }
+        static void Main(string[] args)
+        {
+            int Val = 10;
+            Console.WriteLine("\nValue Before Calling The Function : " + Val);
+            square(ref Val);
+            Console.WriteLine("\nValue After Function Call : " + Val);
+            Console.ReadKey();
+        }
+    }
+}
