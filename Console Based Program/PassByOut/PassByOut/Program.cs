8f8668c763ebfdb8fc50f180b94bd26b47b49f38
LadAkanksha <akankshalad2003@gmail.com>
2024-10-27 21:20:24 +0530

    Console Program

---
+++ Console Based Program/PassByOut/PassByOut/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByOut
+{
+    class Program
+    {
+        static void square(out int val)
+        {
+            val = 5;
+            val = val * val;
+            Console.WriteLine("\nOut Value in Function : " + val);
+        }
+        static void Main(string[] args)
+        {
+            int Val;
+            square(out Val);
+            Console.WriteLine("\nValue After Function Call : " + Val);
+            Console.ReadKey();
+        }
+    }
+}
