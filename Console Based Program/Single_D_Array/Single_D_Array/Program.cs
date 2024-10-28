d762d1c4db26570df4ba3ae079956f6be5acffde
LadAkanksha <akankshalad2003@gmail.com>
2024-10-28 22:44:02 +0530

    Console Program

---
+++ Console Based Program/Single_D_Array/Single_D_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Weekdays;
+            Weekdays = new string[] { "Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday" };
+            foreach (string days in Weekdays)
+            {
+                Console.WriteLine(days);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
