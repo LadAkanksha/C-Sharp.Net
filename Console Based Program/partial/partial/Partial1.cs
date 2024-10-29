9f09e2bfa6600c5bac091e93fb06de2f6cb8c0fd
LadAkanksha <akankshalad2003@gmail.com>
2024-10-29 22:40:12 +0530

    Console Program

---
+++ Console Based Program/partial/partial/Partial1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial
+{
+    public partial class PartialClass
+    {
+        private string firstName;
+        private string lastName;
+        public string FirstName
+        {
+            set
+            {
+                firstName = value;
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                lastName = value;
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+    }
+}
---
+++ Console Based Program/partial/partial/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            PartialClass obj = new PartialClass();
+            obj.FirstName = "Akanksha Lad";
+            obj.LastName = "Lad";
+            Console.WriteLine("Your Full Name is : " + obj.getCompleteName());
+            Console.ReadKey();
+        }
+    }
+}
