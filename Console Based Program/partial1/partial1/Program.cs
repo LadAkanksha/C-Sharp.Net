91bd302d63001c15c1fe7bf7379bd27f9dd70652
LadAkanksha <akankshalad2003@gmail.com>
2024-10-29 22:40:42 +0530

    Console Program

---
+++ Console Based Program/partial1/partial1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial1
+{
+    public partial class PartialClass1
+    {
+        private string firstName;
+        private string lastName;
+        public string FirstName
+        {
+            set
+            {
+                firstName = value;
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                lastName = value;
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+    }
+}
