853a4ad538a3b43fcb1c4d2cac6c8cccd73a5e2f
LadAkanksha <akankshalad2003@gmail.com>
2024-10-29 22:41:50 +0530

    Console Program

---
+++ Console Based Program/partial2/partial2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial1
+{
+    public partial class PartialClass1
+    {
+        public string getCompleteName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+    }
+
+}
