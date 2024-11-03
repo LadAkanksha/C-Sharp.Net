33985a143d962855de34580f8d950a2f4e115d14
LadAkanksha <akankshalad2003@gmail.com>
2024-11-03 22:19:21 +0530

    Console Program

---
+++ Console Based Program/Exception Handling/Null_Reference_Exception/Null_Reference_Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_Reference_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string value = null;
+                if (value.Length == 0)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
