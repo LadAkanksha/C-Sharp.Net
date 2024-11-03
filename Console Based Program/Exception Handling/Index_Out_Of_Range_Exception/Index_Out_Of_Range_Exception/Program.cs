67d98ceb48dc48716ad1dfe5faf317c2e4114cbf
LadAkanksha <akankshalad2003@gmail.com>
2024-11-03 22:20:38 +0530

    Console Program

---
+++ Console Based Program/Exception Handling/Index_Out_Of_Range_Exception/Index_Out_Of_Range_Exception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index_Out_Of_Range_Exception
+{
+    internal class Program : System.Exception
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            try
+            {
+                Console.WriteLine(arr[7]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("An exception has occured" + e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
