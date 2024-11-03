28f9ab338b87ab41ccdf2ec32b1e28d4e0cab88e
LadAkanksha <akankshalad2003@gmail.com>
2024-11-03 22:18:18 +0530

    Console Program

---
+++ Console Based Program/Exception Handling/Divide_By_Zero_Exception/Divide_By_Zero_Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_By_Zero_Exception
+{
+    class Program
+    {
+        int result;
+        Program()
+        {
+            result = 0;
+        }
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught:" + e);
+            }
+            finally
+            {
+                Console.WriteLine("Result : " + result);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program d = new Program();
+            d.division(25, 0);
+            Console.ReadKey();
+        }
+    }
+}
