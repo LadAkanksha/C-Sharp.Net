9982d14a17f94b429e327e97c639e827ee850b6a
LadAkanksha <akankshalad2003@gmail.com>
2024-11-04 22:53:04 +0530

    Console Program

---
+++ Console Based Program/Exception Handling/User_Defined_Exception/User_Defined_Exception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Defined_Exception
+{
+    class UserDefineException : Exception
+    {
+        public UserDefineException(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDefineException M = new UserDefineException("User Define Exception");
+            try
+            {
+                throw M;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
