7bc32d7f6bf03f9b0506ac1153d1d8a9cb1bf3f8
LadAkanksha <akankshalad2003@gmail.com>
2024-11-08 21:51:25 +0530

    Console Program

---
+++ Console Based Program/01_User_Input/01_User_Input/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadKey();
+        }
+    }
+}
