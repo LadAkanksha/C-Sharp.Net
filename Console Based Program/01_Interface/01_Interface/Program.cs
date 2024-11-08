331c87d7106848311e006f981cf6ce6b560fde1a
LadAkanksha <akankshalad2003@gmail.com>
2024-11-08 21:50:12 +0530

    C# Interface

---
+++ Console Based Program/01_Interface/01_Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Interface
+{
+    interface IAnimal
+    {
+        void animalSound(); 
+    }
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();
+        }
+    }
+}
